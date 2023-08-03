66b6772e0d28e27229e621d9e838a586f549702e
Gavin <gavin@limfamily.co>
2023-08-03 05:31:20 -0400

    final update

--- 01_intro/script.js
+++ 01_intro/script.js
@@ -1,15 +1,17 @@
 window.onscroll = function () {
-    var topnav = document.getElementById("myHeader");
+    var topnav = document.getElementById("myTopnav");
     var scrollHeight = window.pageYOffset;
     var opacity = 1 - scrollHeight / 250; // change the denominator to control the speed of fading
 
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topnav.style.opacity = "0";
-    } else {
-        topnav.style.opacity = "1";
-    }
+    // Set the opacity of the top navigation bar
+    topnav.style.opacity = opacity.toFixed(2); // Round to 2 decimal places to avoid flickering
 
-    document.getElementById('scrollMessage').style.opacity = opacity;
+    var scrollMessage = document.getElementById('scrollMessage');
+    if (scrollHeight > 0) {
+        scrollMessage.style.opacity = 0; // Hide the scroll message after scrolling
+    } else {
+        scrollMessage.style.opacity = 1; // Show the scroll message when at the top
+    }
 };
 
 // Function made from ChatGPT
